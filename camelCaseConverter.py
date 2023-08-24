98e842484b139297f879714b42ae38fe30585546
ju957 <ju9572fp@go.minnstate.edu>
2023-08-23 23:32:58 -0500

    Did optional work for camelCaseConverter

--- camelCaseConverter.py
+++ camelCaseConverter.py
@@ -1,3 +1,13 @@
+import string
+
 camelSentence = input("Please enter a sentence to be converted to camelcase. ")
 
-print(camelSentence.title().replace(" ", ""))
+convertedSentence = camelSentence.title().replace(" ", "")
+convertedSentence = convertedSentence[0].lower() + convertedSentence[1:]
+
+print(convertedSentence.find(string.punctuation))
+
+if convertedSentence[0].isdigit() or convertedSentence.find(string.punctuation) != int:
+    print("This will be an invalid variable name.")
+
+print(convertedSentence)
